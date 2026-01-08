285b25844294b7578464adfac3190ec1f99d9d9c
Mark Brooks <Mark.x.brooks@gmail.com>
2026-01-08 00:49:48 -0500

    [refactor] splitting out picomidi

--- picomidi/constant.py
+++ picomidi/constant.py
@@ -12,6 +12,7 @@ from picomidi.messages.pitch_bend import PitchBend
 from picomidi.messages.program_change import ProgramChange
 from picomidi.messages.song import Song
 from picomidi.messages.sysex import SysExByte
+from picomidi.misc import MidiMisc
 
 
 class Midi:
@@ -27,11 +28,4 @@ class Midi:
     PITCH_BEND = PitchBend
     TEMPO = MidiTempo
     CHANNEL = MidiChannel
-
-    NOTES_NUMBER = 128  # Standard MIDI has 128 notes (0-127)
-    TIME_CODE = 0xF1
-    TUNE_REQUEST = 0xF6
-
-    CLOCK = 0xF8
-    ACTIVE_SENSING = 0xFE
-    SYSTEM_RESET = 0xFF
+    MISC = MidiMisc
