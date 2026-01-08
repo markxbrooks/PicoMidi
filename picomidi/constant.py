eba22a88057a28bee46051370a8d8b04542be693
Mark Brooks <Mark.x.brooks@gmail.com>
2026-01-08 01:09:03 -0500

    [refactor] splitting out picomidi

--- picomidi/constant.py
+++ picomidi/constant.py
@@ -1,11 +1,16 @@
 """
 Standard MIDI Protocol Constants
 """
-from picomidi import MidiChannel
+from picomidi.core.channel_legacy import MidiChannel
 from picomidi.core.tempo import MidiTempo
 from picomidi.core.value import MidiValue
-from picomidi.messages import (Aftertouch, ControlChange, MidiNote, PitchBend,
-                               ProgramChange, Song, SysExByte)
+from picomidi.messages.aftertouch import Aftertouch
+from picomidi.messages.control_change import ControlChange
+from picomidi.messages.note import MidiNote
+from picomidi.messages.pitch_bend import PitchBend
+from picomidi.messages.program_change import ProgramChange
+from picomidi.messages.song import Song
+from picomidi.messages.sysex import SysExByte
 from picomidi.misc import MidiMisc
 
 
