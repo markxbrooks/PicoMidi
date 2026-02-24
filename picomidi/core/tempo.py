5f53106e38c2d3d81e4c99841dc800ef277019c4
Mark Brooks <Mark.x.brooks@gmail.com>
2026-02-24 13:33:11 -0500

    [method] added milliseconds_per_note

--- picomidi/core/tempo.py
+++ picomidi/core/tempo.py
@@ -16,3 +16,19 @@ class MidiTempo:
     MILLISECONDS_PER_SECOND = 1_000
     MILLISECONDS_PER_MINUTE = 60_000
     SECONDS_PER_MINUTE = 60
+
+
+def milliseconds_per_note(bpm: int | float, division: int = 4) -> float:
+    """calculate step duration
+    1 = quarter note
+    2 = eighth note
+    4 = 16th note
+    """
+    if bpm is None:
+        raise ValueError("BPM must not be None")
+    if bpm <= 0:
+        raise ValueError("BPM must be positive")
+    if division <= 0:
+        raise ValueError("division must be positive")
+    ms_per_step = (MidiTempo.MILLISECONDS_PER_MINUTE / bpm) / division  # 4 = ms per 16th note
+    return ms_per_step
